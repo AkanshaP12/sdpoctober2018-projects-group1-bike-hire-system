139b5d44329934f354300030bf4f9435bffd1dd1
Virajkumar Patel <viraj.motog@gmail.com>
2018-11-26 12:51:51 +0100

    Defined project structure

---
+++ src/main/java/edu/srh/bikehire/util/DateUtil.java
@@ -0,0 +1,12 @@
+package edu.srh.bikehire.util;
+
+import java.util.Calendar;
+
+public class DateUtil {
+	
+	public static Calendar getCalendarFromString(String pDateInString)
+	{
+		return Calendar.getInstance();
+	}
+	
+}

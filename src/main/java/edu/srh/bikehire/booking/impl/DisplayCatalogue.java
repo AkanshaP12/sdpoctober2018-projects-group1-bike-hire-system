cd18d134ad182ef4924474b7b15f606408d1deac
Unknown <gautamr2590@gmail.com>
2018-11-27 15:30:08 +0100

    #14 Added interface for booking entities

--- src/main/java/edu/srh/bikehire/booking/impl/DisplayCatalogue.java
+++ src/main/java/edu/srh/bikehire/booking/impl/DisplayCatalogue.java
@@ -1,5 +1,36 @@
 package edu.srh.bikehire.booking.impl;
 
-public class DisplayCatalogue {
+import edu.srh.bikehire.booking.Booking;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//MyGETRequest
+public class DisplayCatalogue implements Booking {
+	public static void main(String args[]) throws IOException {
+	    URL urlForGetRequest = new URL("https://jsonplaceholder.typicode.com/posts/1/comments");
+	    String readLine = null;
+	    HttpURLConnection conn = (HttpURLConnection) urlForGetRequest.openConnection();
+	    conn.setRequestMethod("GET");
+	    
+	    int responseCode = conn.getResponseCode();
+	    if (responseCode == HttpURLConnection.HTTP_OK) {
+	        BufferedReader in = new BufferedReader(
+	            new InputStreamReader(conn.getInputStream()));
+	        StringBuffer response = new StringBuffer();
+	        while ((readLine = in .readLine()) != null) {
+	            response.append(readLine);
+	        } in .close();
+	        
+	        System.out.println("JSON String Result " + response.toString());
+	        
+	    } else {
+	        System.out.println("Error!");
+	    }
+	}
+
+	
 }

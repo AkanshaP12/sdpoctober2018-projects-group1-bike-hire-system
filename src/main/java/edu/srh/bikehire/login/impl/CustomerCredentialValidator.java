d06833845ccdc9ccd45b78ecb32b6530ab2701d4
Virajkumar Patel <viraj.motog@gmail.com>
2018-11-28 19:08:44 +0100

    Adding Credential Validator

--- src/main/java/edu/srh/bikehire/login/impl/CustomerCredentialValidator.java
+++ src/main/java/edu/srh/bikehire/login/impl/CustomerCredentialValidator.java
@@ -1,6 +1,16 @@
 package edu.srh.bikehire.login.impl;
 
+import edu.srh.bikehire.login.EntityLoginCredential;
+
 public class CustomerCredentialValidator {
+	
+	//MEMBERS
+	EntityLoginCredential mEntityLoginCredential = null;
+	
+	public CustomerCredentialValidator(EntityLoginCredential pEntityLoginCredential)
+	{
+		mEntityLoginCredential = pEntityLoginCredential;
+	}
 	
 	
 	

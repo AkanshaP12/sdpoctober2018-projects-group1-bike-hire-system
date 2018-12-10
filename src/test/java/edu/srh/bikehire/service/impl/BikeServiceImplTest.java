3ee48d2a234a50571e878e6c58195ef724342c95
mithun93 <reachmithun.n@gmail.com>
2018-12-10 15:48:49 +0100

    commiting code

--- src/test/java/edu/srh/bikehire/service/impl/BikeServiceImplTest.java
+++ src/test/java/edu/srh/bikehire/service/impl/BikeServiceImplTest.java
@@ -16,17 +16,11 @@ import edu.srh.bikehire.exception.BikeHireSystemException;
 import edu.srh.bikehire.service.core.impl.BikeStatusInfo;
 
 public class BikeServiceImplTest {
-	
+
 	BikeServiceImpl bikeServiceImplTest = new BikeServiceImpl();
 
 	@Test
-	public void testInitializeService() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetAllBikes() throws BikeHireSystemException
-	{
+	public void testGetAllBikes() throws BikeHireSystemException {
 		boolean sortDescending = true;
 		List<BikeDTO> getBikes = bikeServiceImplTest.getAllBikes(sortDescending);
 		assertNotNull(getBikes);
@@ -34,136 +28,212 @@ public class BikeServiceImplTest {
 	}
 
 	@Test
-	public void testGetBikeByID() throws BikeHireSystemException
-	{
+	public void testGetBikeByID() throws BikeHireSystemException {
 		BikeDTO BikeReturn = bikeServiceImplTest.getBikeByID(1);
 		assertNotNull(BikeReturn);
 	}
-	
+
 	@Test(expected = BikeHireSystemException.class)
-	public void testGetBikeByIDInvalid() throws BikeHireSystemException
-	{
+	public void testGetBikeByIDInvalid() throws BikeHireSystemException {
 		BikeDTO BikeReturnInvalid = bikeServiceImplTest.getBikeByID(-1);
-		
-	}
-	
-	@Test
-	public void testUpdateBikeStatus() throws BikeHireSystemException
-	{
-		BikeStatusType.AVALIABLE_BIKE.getBikeStatus();
-		BikeStatusType.RENTED_BIKE.getBikeStatus();
-		BikeStatusType.UNDERMAINTAINCE_BIKE.getBikeStatus();
-		
-	}
-	
-	@Test
-	public void testGetAllBikesBasedOnType() throws BikeHireSystemException
-	{	
-	    boolean sortDescending = true;
-		List<BikeDTO> basedOnType = bikeServiceImplTest.getAllBikesBasedOnType(1,sortDescending);
-		
-		
 	}
 
 	@Test
-	public void testGetAllBikesBasedOnWarehouse() throws BikeHireSystemException
-	{
-		
+	public void testUpdateBikeStatus() throws BikeHireSystemException {
+		bikeServiceImplTest.updateBikeStatus(1, BikeStatusType.AVALIABLE_BIKE);
+	}
+
+	@Test(expected = BikeHireSystemException.class)
+	public void testUpdateBikeStatusInvalid() throws BikeHireSystemException {
+		bikeServiceImplTest.updateBikeStatus(1, null);
+	}
+
+	@Test(expected = BikeHireSystemException.class)
+	public void testUpdateBikeStatusInvalidBikeId() throws BikeHireSystemException {
+		bikeServiceImplTest.updateBikeStatus(-1, BikeStatusType.AVALIABLE_BIKE);
+	}
+
+	@Test
+	public void testGetAllBikesBasedOnType() throws BikeHireSystemException {
+		boolean sortDescending = true;
+		List<BikeDTO> basedOnType = bikeServiceImplTest.getAllBikesBasedOnType(1, sortDescending);
+		assertNotNull(basedOnType);
+		assertTrue(basedOnType.size() > 0);
+	}
+
+	@Test
+	public void testGetAllBikesBasedOnWarehouse() throws BikeHireSystemException {
+
 		boolean sortPriceDescending = true;
 		List<BikeDTO> bikesbasedonwarehouse = bikeServiceImplTest.getAllBikesBasedOnWarehouse(1, sortPriceDescending);
 		assertNotNull(bikesbasedonwarehouse);
 		assertTrue(bikesbasedonwarehouse.size() > 0);
 	}
 
-	
 	@Test(expected = BikeHireSystemException.class)
-	public void testGetAllBikesBasedOnWarehouseInvalid() throws BikeHireSystemException
-	{
-		
+	public void testGetAllBikesBasedOnWarehouseInvalid() throws BikeHireSystemException {
+
 		boolean sortPriceDescending = false;
 		List<BikeDTO> bikesbasedonwarehouse = bikeServiceImplTest.getAllBikesBasedOnWarehouse(-1, sortPriceDescending);
-		
+
 	}
-	
-	
+
 	@Test
-	public void testGetBikeTypeInfo() throws BikeHireSystemException
-	{
+	public void testGetBikeTypeInfo() throws BikeHireSystemException {
 		BikeTypeDTO biketypeinfo = bikeServiceImplTest.getBikeTypeInfo(1);
 		assertNotNull(biketypeinfo);
 	}
 
 	@Test(expected = BikeHireSystemException.class)
-	public void testGetBikeTypeInfoinvalid() throws BikeHireSystemException
-	{
+	public void testGetBikeTypeInfoinvalid() throws BikeHireSystemException {
 		BikeTypeDTO biketypeinfo = bikeServiceImplTest.getBikeTypeInfo(-1);
-		
+
 	}
-	
-	
+
 	@Test
-	public void testGetBikeTypes() throws BikeHireSystemException
-	{
+	public void testGetBikeTypes() throws BikeHireSystemException {
 		List<BikeTypeDTO> biketype = bikeServiceImplTest.getBikeTypes();
 		assertNotNull(biketype);
 		assertTrue(biketype.size() > 0);
 	}
 
 	@Test
-	public void testGetAllBikesBasedOnStatus() throws BikeHireSystemException
-	{
-		
-		List<BikeDTO> bikestatus = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.AVALIABLE_BIKE, true);
-		assertNotNull(bikestatus);
-		assertTrue(bikestatus.size() > 0);
-		
-		List<BikeDTO> bikestatusrented = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.RENTED_BIKE, true);
-		assertNotNull(bikestatusrented);
-		assertTrue(bikestatusrented.size() > 0);
-		
-		List<BikeDTO> bikestatusunderservice = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.UNDERMAINTAINCE_BIKE, true );
-		assertNotNull(bikestatusunderservice);
-		assertTrue(bikestatusunderservice.size() > 0);
-	}
-	
-	@Test(expected = BikeHireSystemException.class)
-	public void testGetAllBikesBasedOnStatusinvalid() throws BikeHireSystemException
-	{
-		
-		List<BikeDTO> bikestatus = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.AVALIABLE_BIKE, false);
-		assertNotNull(bikestatus);
-		assertTrue(bikestatus.size() > 0);
-		
-		List<BikeDTO> bikestatusrented = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.RENTED_BIKE, false);
-		assertNotNull(bikestatusrented);
-		assertTrue(bikestatusrented.size() > 0);
-		
-		List<BikeDTO> bikestatusunderservice = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.UNDERMAINTAINCE_BIKE, false );
-		assertNotNull(bikestatusunderservice);
-		assertTrue(bikestatusunderservice.size() > 0);
+	public void testGetAllBikesBasedOnStatus() throws BikeHireSystemException {
+
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.AVALIABLE_BIKE, true);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = Integer.MAX_VALUE;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() > previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
 	}
 
 	@Test
-	public void testGetBikeRent() throws BikeHireSystemException
-	{
+	public void testGetAllBikesBasedOnStatusrent() throws BikeHireSystemException {
+
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.RENTED_BIKE, true);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = Integer.MAX_VALUE;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() > previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
+
+	}
+
+	@Test
+	public void testGetAllBikesBasedOnStatusrentmaintainance() throws BikeHireSystemException {
+
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.UNDERMAINTAINCE_BIKE, true);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = Integer.MAX_VALUE;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() > previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
+
+	}
+
+	@Test(expected = BikeHireSystemException.class)
+	public void testGetAllBikesBasedOnStatusinvalidavaliable() throws BikeHireSystemException {
+
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.AVALIABLE_BIKE, false);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = 0;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() < previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
+	}
+
+	@Test(expected = BikeHireSystemException.class)
+	public void testGetAllBikesBasedOnStatusinvalidrented() throws BikeHireSystemException {
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.RENTED_BIKE, false);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = 0;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() < previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
+	}
+
+	@Test(expected = BikeHireSystemException.class)
+	public void testGetAllBikesBasedOnStatusinvalidmaintaince() throws BikeHireSystemException {
+		List<BikeDTO> bikes = bikeServiceImplTest.getAllBikesBasedOnStatus(BikeStatusType.UNDERMAINTAINCE_BIKE, false);
+		assertNotNull(bikes);
+		assertTrue(bikes.size() > 0);
+		boolean isCorrectOrder = true;
+		int previousDeposit = 0;
+		for (BikeDTO bikeDTO : bikes) {
+			if (bikeDTO.getDepositAmount() < previousDeposit) {
+				isCorrectOrder = false;
+				break;
+			} else {
+				previousDeposit = bikeDTO.getDepositAmount();
+			}
+		}
+
+		assertTrue(isCorrectOrder);
+	}
+
+	@Test
+	public void testGetBikeRent() throws BikeHireSystemException {
 		BikeRentMappingDTO bikerent = bikeServiceImplTest.getBikeRent(1);
 		assertNotNull(bikerent);
 	}
-	
+
 	@Test(expected = BikeHireSystemException.class)
-	public void testGetBikeRentinvalid() throws BikeHireSystemException
-	{
+	public void testGetBikeRentinvalid() throws BikeHireSystemException {
 		BikeRentMappingDTO bikerent = bikeServiceImplTest.getBikeRent(-1);
 	}
-	
 
 	@Test
-	public void testGetAllWarehouses() throws BikeHireSystemException
-	{
+	public void testGetAllWarehouses() throws BikeHireSystemException {
 		List<WareHouseDTO> allWarehouses = bikeServiceImplTest.getAllWarehouses();
 		assertNotNull(allWarehouses);
 		assertTrue(allWarehouses.size() > 0);
-		
+
 	}
 
 }
